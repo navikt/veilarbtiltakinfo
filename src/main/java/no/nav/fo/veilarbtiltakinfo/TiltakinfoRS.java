b73f06423bb3f9735ad777c2436beb2ed94958d5
Johan Rusvik <johan.rusvik@nav.no>
2018-10-11 12:11:33 +0200

    KAIZEN-0 Sjekker leserettigheter på bruker

--- src/main/java/no/nav/fo/veilarbtiltakinfo/TiltakinfoRS.java
+++ src/main/java/no/nav/fo/veilarbtiltakinfo/TiltakinfoRS.java
@@ -1,25 +1,47 @@
 package no.nav.fo.veilarbtiltakinfo;
 
+import no.nav.apiapp.security.PepClient;
+import no.nav.common.auth.SubjectHandler;
 import no.nav.fo.veilarbtiltakinfo.client.OppfolgingStatus;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import static java.util.Optional.ofNullable;
 
 @Component
 @Path("/")
 @Produces("application/json")
 public class TiltakinfoRS {
 
+    public static final String FNR_QUERY_PARAM = "fnr";
+
+    @Inject
+    private Provider<HttpServletRequest> requestProvider;
+
     @Inject
     private TiltakService tiltakService;
+
+    @Inject
+    private PepClient pepClient;
 
     @GET
     @Path("servicegruppekode")
     public OppfolgingStatus hentServiceGruppeKode() {
-        return tiltakService.hentServiceGruppe();
+        String fnr = getFnr();
+        pepClient.sjekkLeseTilgangTilFnr(fnr);
+        return tiltakService.hentServiceGruppe(fnr);
     }
 
+    private String getFnr() {
+        String fnr = requestProvider.get().getParameter(FNR_QUERY_PARAM);
+        return ofNullable(fnr).orElseGet(() ->
+            SubjectHandler.getIdent().orElseThrow(IllegalArgumentException::new)
+        );
+    }
 }
