70afad7dc01f85223d2b43df18d105787f1f1d74
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2019-05-15 10:36:26 +0200

    PUS-232: Fikser manglende config av AktorService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,15 @@ import no.nav.fo.veilarbtiltakinfo.config.ApplicationConfig;
 
 import static java.lang.System.setProperty;
 import static no.nav.brukerdialog.security.Constants.OIDC_REDIRECT_URL_PROPERTY_NAME;
+import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.fo.veilarbtiltakinfo.config.ApplicationConfig.VEILARBLOGIN_REDIRECT_URL_URL;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
     public static void main(String... args) throws Exception {
+
+        setProperty(AKTOER_ENDPOINT_URL, ApplicationConfig.getAktorEndpointUrl());
+
         setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty(VEILARBLOGIN_REDIRECT_URL_URL));
 
         ApiApp.runApp(ApplicationConfig.class, args);
